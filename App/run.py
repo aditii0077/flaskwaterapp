7065b1d104b654764366173f559f9ee6cba41569
aditii0077 <aditii0077@gmail.com>
2024-12-26 20:29:53 +0530

    Fix app module import

--- App/run.py
+++ App/run.py
@@ -1,11 +1,7 @@
 import sys
 import os
 
-# Add the current directory to the sys.path to allow relative imports
+# Add the parent directory (App) to sys.path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from app import app
-from app import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
